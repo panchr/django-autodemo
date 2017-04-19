1d3cd6182b351567da29a6e5539eeb5fe11e1534
Rushy Panchal <panchal.rushy@gmail.com>
2017-04-19 14:09:08 -0400

    Start initial demo backend implementation

---
+++ django_demo_mode/backends.py
@@ -0,0 +1,39 @@
+# django_demo_mode/backends.py
+# django-demo-mode
+# Author: Rushy Panchal
+# Date: April 19th, 2017
+# Description: Authentication backends for django-demo-mode.
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.utils.crypto import get_random_string
+from django.db import IntegrityError
+
+class DemoBackend(object):
+	def authenticate(self, request=None):
+		'''
+		Attempt to generate a random user for the request.
+		'''
+		user = None
+		attempts = 0
+		while user is None and attempts < 10:
+			try:
+				user = User.objects.create(username=get_random_string(150))
+			except IntegrityError:
+				attempts += 1
+
+		return user
+
+	def get_user(self, user_id):
+		'''
+		Get the user given the user's ID.
+
+		:param user_id: user's ID
+
+		:return: User object or None if not found
+		:rtype: django.contrib.auth.models.User
+		'''
+		try:
+			return User.objects.get(pk=user_id)
+		except User.DoesNotExist:
+			return None
